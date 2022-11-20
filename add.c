8b02c6e06a4a0e181ad9be52c92bbfb73337e95a
khokan_gorain <khokangorain24hour@gmail.com>
2022-11-20 18:49:46 +0530

    This is initial commit

---
+++ add.c
@@ -0,0 +1,12 @@
+void main()
+{
+    int a,b,c=0;
+    printf("Enter the first number:");
+    scanf("%d ",&a);
+    printf("Enter the second number:");
+    scanf("%d ",&b);
+
+    c = a+b;
+    printf("Sum is %d ",c);
+    
+}
