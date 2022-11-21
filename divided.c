8993766b38b73ab1e142f70b5df06ef81fb8dddb
khokan_gorain <khokangorain24hour@gmail.com>
2022-11-21 20:17:28 +0530

    This is divided file

---
+++ divided.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+
+    printf("Divided is %d",a/b);
+}
