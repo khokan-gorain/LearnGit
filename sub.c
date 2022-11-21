da57f3d5002667b758f84dde7feaccac4e7c549d
khokan_gorain <khokangorain24hour@gmail.com>
2022-11-21 20:08:04 +0530

    This is subtaraction program

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c=0;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+
+    c = a-b;
+    prinft("Subraction is %d",c);
+}
