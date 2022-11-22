ecba87c570a201b596444991f58e13cabf82fc31
khokan_gorain <khokangorain24hour@gmail.com>
2022-11-22 08:58:14 +0530

    This is multiplaction file

---
+++ mul.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,m=1;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+
+    printf("Multiplication is %d",a*b);
+}
